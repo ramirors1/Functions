37ce2c6ab539d86fb47cf12773ea1bbeed77824b
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-09-14 15:04:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#def greeting():
+ #   print("Hello")
+  #  print("Shello")
+   # print("Sup dude")
+
+#greeting()
+
+#def greet_with_name(name):
+ #   print(f"How's it going {name}")
+
+#greet_with_name("Jackie boy")
+
+def greet_with(name, location):
+    print(f"Hi {name}")
+    print(f"How is it in {location}")
+
+greet_with(location="miami", name="Scott")
